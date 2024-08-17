daf65ea57a77fe9b7317b5953f0b6efb1848822e
shahin009 <shaheenmis170879@gmail.com>
2024-08-17 16:22:21 +0600

    git updated

--- public/gi.js
+++
@@ -1,7 +0,0 @@
-{/* <button
-                    onClick={() => setAsc(!asc)}
-                    className="btn bg-pink-500">
-                    {
-                        asc ? 'Price: High to Low' : 'Price: Low to High'
-                    }
-                </button> */}
